7aea741294c726ce4e7221b5d11eae3a764059b4
dannycahyo <dannydwicahyono@gmail.com>
2024-07-07 12:36:16 +0700

    chore: Add initial project files and configuration

---
+++ src/main/java/dannydwicahyono/spring_commerce/SpringCommerceApplication.java
@@ -0,0 +1,13 @@
+package dannydwicahyono.spring_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dannydwicahyono/spring_commerce/SpringCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package dannydwicahyono.spring_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
